74c9570c6df2a82577b9a28b6486dfc8c35c6d59
Erick Echeverría <erickecheverria66@gmail.com>
2021-06-14 09:51:51 -0600

    Inicializacion del proyecto y conexion a la base de datos

---
+++ SistemaFacturacionMVC/DB/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Creado y modificado por: Erick Eduardo Echeverría Garrido - 14/06/2021
+
+namespace SistemaFacturacionMVC.DB
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
