8e5f2c69bf694ca3ca8e1547d171fc695b318d31
Erick Echeverría <erickecheverria66@gmail.com>
2021-06-24 01:48:24 -0600

    Pagina de inicio terminada

--- SistemaFacturacionMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SistemaFacturacionMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -62,7 +62,7 @@ namespace SistemaFacturacionMVC.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "La {0} debe de tener al menos {2} y un máximo de {1} caracteres.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Contraseña")]
             public string Password { get; set; }
