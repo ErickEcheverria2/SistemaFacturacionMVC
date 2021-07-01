de824f3764d8b7adcd4401347f4ae90c65314523
Erick Echeverría <erickecheverria66@gmail.com>
2021-07-01 17:20:59 -0600

    Cambios pequeños

--- SistemaFacturacionMVC/Controllers/UsuariosController.cs
+++ SistemaFacturacionMVC/Controllers/UsuariosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SistemaFacturacionMVC.Areas.Identity.Data;
 using SistemaFacturacionMVC.Data;
 using SistemaFacturacionMVC.DB;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace SistemaFacturacionMVC.Controllers
 {
+    [Authorize]
     public class UsuariosController : Controller
     {
         private readonly AuthDbContext _context;
